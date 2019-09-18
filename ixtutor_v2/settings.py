dd4dbac80885bb5940eaeccd738735562b769f31
Yuriy Malkov <yuriy.malkov@outlook.com>
2019-09-17 22:24:06 -0400

    added django_heroku module

--- ixtutor_v2/settings.py
+++ ixtutor_v2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -86,7 +87,6 @@ DATABASES = {
         'OPTIONS':  {
             'sslmode': True
         }
-
     }
 }
 
@@ -129,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
