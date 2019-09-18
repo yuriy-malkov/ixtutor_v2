ee5317e4cee8505c6bbf9eee64a6251873d09bd7
desaivraj <55449940+desaivraj@users.noreply.github.com>
2019-09-18 11:40:10 -0400

    Update urls.py

--- tutor/urls.py
+++ tutor/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
+    path('tutor/Booking', views.tutorBooking, name='tutorBooking'),
     path('user/viewAllBookings', views.view_all_bookings, name='viewAllBookings')
 ]
