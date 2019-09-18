b567502ab6889e316aab661fc461953da775cc3a
Yuriy Malkov <yuriy.malkov@outlook.com>
2019-09-17 21:33:28 -0400

    added psycopg2 module to project for db

--- ixtutor_v2/settings.py
+++ ixtutor_v2/settings.py
@@ -74,12 +74,16 @@ WSGI_APPLICATION = 'ixtutor_v2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tutor_db',
+        'USER': 'tutor',
+        'PASSWORD': 'tutor',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
