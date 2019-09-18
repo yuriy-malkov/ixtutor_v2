7c8292eb8d6348022f896aa8140736dbafa217c4
Thomas Hardy <thomas.hardy@indexexchange.com>
2019-09-18 16:36:00 -0400

    Styling registration

--- tutor/views.py
+++ tutor/views.py
@@ -5,26 +5,177 @@ from .forms import RegisterForm
 from .models import Users
 
 bookings = [{
-    'tutorID': 1,
-    'bookingID': 20,
-    'interestID': 30,
-    'roomID': 40,
-    'slotID': 50
-},
-    {
-        'tutorID': 2,
-        'bookingID': 30,
-        'interestID': 40,
-        'roomID': 50,
-        'slotID': 60
+            'tutorID' : 1,
+            'bookingID' : 1, 
+            'interestID' : 1,
+            'roomID' : 3, 
+            'slotID' : 2
+        },
+        {
+            'tutorID' : 2,
+            'bookingID': 2,
+            'interestID': 2,
+            'roomID': 2, 
+            'slotID': 2
+        },
+        {
+            'tutorID' : 3,
+            'bookingID': 3,
+            'interestID': 1,
+            'roomID': 3, 
+            'slotID': 1
+        }]
+
+
+interests = [{
+    'interestID' : 1,
+    'name' : "Identity"
     },
     {
-        'tutorID': 3,
-        'bookingID': 15,
-        'interestID': 25,
-        'roomID': 35,
-        'slotID': 45
-    }]
+    'interestID' : 2,
+    'name' : "Python"
+    },
+    {
+    'interestID' : 3,
+    'name' : "Wrapper"
+    }
+]
+
+rooms = [{
+    'roomID' : 1,
+    'name' : "Room A"
+    },
+    {
+    'roomID' : 2,
+    'name' : "Room B"   
+    },
+    {
+    'roomID' : 3,
+    'name' : "Room C"   
+    }
+]
+
+timeSlots = [{
+    'timeSlotID' : 1,
+    'name' : '10AM-11AM'
+    },
+    {
+    'timeSlotID' : 2,
+    'name' : '2PM-3PM'
+    },
+    {
+    'timeSlotID' : 3,
+    'name' : '12PM-1PM'
+    }
+]
+
+days = [{
+    'dayID' : 1,
+    'name' : "Monday"
+    },
+    {
+    'dayID' : 2,
+    'name' : "Wednesday"
+    },
+    {
+    'dayID' : 1,
+    'name' : "Friday"
+    }
+]
+
+availability = [{
+    'bookingID' : 1,
+    'isAvailable' : 1,
+    'dayID' : 1,
+    'roomID' : 2,
+    'timeSlotID' : 3
+    },
+    {
+    'bookingID' : 2,
+    'isAvailable' : 1,
+    'dayID' : 1,
+    'roomID' : 2,
+    'timeSlotID' : 2
+    },
+    {
+    'bookingID' : 3,
+    'isAvailable' : 0,
+    'dayID' : 3,
+    'roomID' : 2,
+    'timeSlotID' : 1
+    }
+]
+
+users = [{
+    'userID' : 1,
+    'email' : 'email1@gmail.com',
+    'status' : 1,
+    'password' : "password",
+    'isTutor' : 0,
+    'isStudent': 0,
+    },
+    {
+    'userID' : 2,
+    'email' : 'email2@gmail.com',
+    'status' : 0,
+    'password' : "password",
+    'isTutor' : 1,
+    'isStudent': 0,
+    },
+    {
+    'userID' : 3,
+    'email' : 'email3@gmail.com',
+    'status' : 1,
+    'password' : "password",
+    'isTutor' : 0,
+    'isStudent': 1,
+    }
+]
+
+userInterests = [{
+    'userID' : 1,
+    'interestID' : 1 
+    },
+    {
+    'userID' : 2,
+    'interestID' : 2
+    },
+    {
+    'userID' : 3,
+    'interestID' : 3 
+    }
+]
+
+tutorBookings = [{
+    'bookingID' : 1,
+    'userID' : 1,
+    'interestID' : 1
+    },
+    {
+    'bookingID' : 1,
+    'userID' : 3,
+    'interestID' : 2
+    },
+    {
+    'bookingID' : 1,
+    'userID' : 2,
+    'interestID' : 3
+    }
+]
+
+studentEnroll = [{
+    'userID' : 1,
+    'enrollID' : 2
+    },
+    {
+    'userID' : 3,
+    'enrollID' : 1
+    },
+    {
+    'userID' : 2,
+    'enrollID' : 3
+    }
+]
 
 
 # Create your views here.
