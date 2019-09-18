49a41c5b98b04963ad18f512365ccd3c67cb7034
Yuriy Malkov <yuriy.malkov@outlook.com>
2019-09-17 22:02:16 -0400

    added postgres connection for django/heroku

--- ixtutor_v2/settings.py
+++ ixtutor_v2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +86,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
