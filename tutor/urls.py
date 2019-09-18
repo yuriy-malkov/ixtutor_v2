9814ac5a2113fe07469fd80d618dcc3d71ad2b81
Archan Brahmbhatt <archan.brahmbhatt@mac-1598ab.local>
2019-09-18 12:10:29 -0400

    tutor bookings:

--- tutor/urls.py
+++ tutor/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
-    path('tutor/Booking', views.tutorBooking, name='tutorBooking'),
+    path('tutor/Booking', views.tutor_bookings, name='tutor_bookings'),
     path('user/viewAllBookings', views.view_all_bookings, name='viewAllBookings')
 ]
