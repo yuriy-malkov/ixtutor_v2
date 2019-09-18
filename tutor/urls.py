0d1d06cfa7b9fea538521edb2a979259b89fa29f
Thomas Hardy <thomas.hardy@indexexchange.com>
2019-09-18 12:30:42 -0400

    2 routes

--- tutor/urls.py
+++ tutor/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
-    path('user/viewAllBookings', views.view_all_bookings, name='viewAllBookings')
+    path('user/viewAllBookings', views.view_all_bookings, name='viewAllBookings'),
+    path(r'user/bookings/<int:bookingID>', views.booking_information, name='bookingInformation')
 ]
